78bf20285937867aa6b818be989b5ef8b49d4fe6
c-y-b-o-r-g-g <es-jihad.musa2026@alexu.edu.eg>
2024-08-30 18:35:20 +0300

    Reverse Words

--- reverseWords.py
+++ reverseWords.py
@@ -1,8 +1,16 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(" ")
+        words.reverse()
+        out = ""
+        # print(words)
+        for word in words:
+            if word !="" and word!=" ":
+                out+=word
+                out+=" "
+        return out.strip()
+        
 
-        
-                
-        
-        
 sol = Solution()
 res = sol.reverseWords("a good   example")
 print(res)
